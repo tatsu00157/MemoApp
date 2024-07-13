32b4397b9b99c929194a16f6d363e66ec09a7d0d
kalan <kawaitatsuya860@gmail.com>
2024-07-14 02:42:44 +0900

    UI Component add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,54 +1,20 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, StyleSheet } from "react-native"
+
+import Header from "../components/header"
+import MemoListItem from "../components/memoListItem"
+import CircleButton from "../components/circleButton"
 
 const Index = (): JSX.Element => {
     return (
-        <View style={styles.container}>
-
-            <View style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-            </View>
-
-            <View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年10月1日</Text>
-                    </View>
-                    <View>
-                        <Text>×</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年10月1日</Text>
-                    </View>
-                    <View>
-                        <Text>×</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2024年10月1日</Text>
-                    </View>
-                    <View>
-                        <Text>×</Text>
-                    </View>
-                </View>
-
-            </View>
-
-            <View style={styles.circleBuoton}>
-                <Text style={styles.circleButtonLabel}>+</Text>
-            </View>
+    <View style={styles.container}>
+           <Header />
+        <View>
+            <MemoListItem />
+            <MemoListItem />
+            <MemoListItem />
         </View>
+        <CircleButton>+</CircleButton>
+    </View>
     )
 }
 
@@ -57,72 +23,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'ffffff'
-    },
-    // ヘッダー
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end'
-    },
-    headerInner: {
-        alignItems: 'center'
-    },
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255,255,255,0.7)'
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff'
-    },
-    // メモリスト
-    memoListItem: {
-        backgroundColor: 'ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderColor: 'rgba(0,0,0,0.15)'
-    },
-    memoListItemTitle: {
-        fontSize: 16,
-        lineHeight: 32
-    },
-    memoListItemDate: {
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484'
-    },
-    // ボタン
-    circleBuoton: {
-        width: 64,
-        height: 64,
-        borderRadius: 32,
-        backgroundColor: '#467FD3',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right: 40,
-        bottom: 40,
-        shadowColor: '#000000',
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-        textShadowOffset: { width: 0, height: 8 },
-        elevation: 8
-    },
-    circleButtonLabel: {
-        color: '#ffffff',
-        fontSize: 40,
-        lineHeight: 48
-    },
-
+    }
 })
 
 export default Index
