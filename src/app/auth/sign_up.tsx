3c4f351274507e2c48b437080d6149c9817223ee
kalan <kawaitatsuya860@gmail.com>
2024-07-18 00:34:10 +0900

    sign up

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,16 +1,29 @@
 import {
-     View, Text, TextInput,
+     View, Text, TextInput, Alert,
      TouchableOpacity, StyleSheet
      } from "react-native"
 
 import { Link, router } from "expo-router"
 import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 import Button from "../../components/button"
+import { auth } from "../../config"
 
-const handlePress = (): void => {
+const handlePress = (email: string, password:string): void => {
     // 会員登録
-    router.push('/memo/list')
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredintial) => {
+            console.log(userCredintial.user.uid)
+            router.replace('/memo/list')
+        })
+        .catch((error) => {
+            const {code, message} = error
+            console.log(code, message)
+            Alert.alert(message)
+        })
+    
 }
 
 const SignUp = (): JSX.Element => {
@@ -39,7 +52,7 @@ const SignUp = (): JSX.Element => {
              placeholder="Password"
              textContentType="password"
              />
-            <Button label= 'Submit' onPress={handlePress}/>
+            <Button label= 'Submit' onPress={() => {handlePress(email, password) }}/>
              <View style={styles.footer}>
                 <Text style={styles.footerText}>Already registered?</Text>
                 <Link href='/auth/log_in' asChild>
