8a6cd840a5d4e206ab0a703bb6f0dd5ed0371e1b
kalan <kawaitatsuya860@gmail.com>
2024-07-26 00:17:54 +0900

    Detail Display

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, FlatList } from "react-native"
 import { router, useNavigation } from "expo-router"
 import { useEffect, useState } from "react"
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore"
@@ -29,7 +29,6 @@ const List = (): JSX.Element => {
         const unsubscribe = onSnapshot(q,(snapshot) => {
             const remoteMemos: Memo[] = []
             snapshot.forEach((doc) => {
-                console.log('memo', doc.data())
                 const { bodyText, updatedAt} = doc.data()
                 remoteMemos.push({
                     id: doc.id,
@@ -43,10 +42,11 @@ const List = (): JSX.Element => {
     }, [])
     return (
     <View style={styles.container}>
-           
-        <View>
-            {memos.map((memo) => <MemoListItem memo={memo} />)}
-        </View>
+           <FlatList
+           data={memos}
+           renderItem={({ item}) =>  <MemoListItem memo={item} />}
+            />
+        
         <CircleButton onPress={handlePress}>
             <Icon name="plus" size={40} color="#ffffff"/>
         </CircleButton>
