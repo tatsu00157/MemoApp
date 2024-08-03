dd2d677828f3595badec585290a6e056e7feb458
kalan <kawaitatsuya860@gmail.com>
2024-08-04 01:52:41 +0900

    MemoList Delete

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -8,16 +8,16 @@ import Icon from "../../components/Icon"
 import { auth, db } from "../../config"
 import { type Memo } from "../../../types/memo"
 
-const handlePress = (): void => {
-    router.push('/memo/edit')
+const handlePress = (id: string): void => {
+    router.push({ pathname: '/memo/edit', params: { id }})
 }
 
 const Detail = (): JSX.Element => {
-    const { id } = useLocalSearchParams()
+    const id = String(useLocalSearchParams().id)
     const [memo, setMemo] = useState<Memo | null>(null)
     useEffect(() => {
         if (auth.currentUser === null) { return }
-        const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id))
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
         const unsubscribe = onSnapshot(ref, (memoDoc) => {
             const { bodyText, updatedAt } = memoDoc.data() as Memo
             setMemo({
@@ -41,7 +41,7 @@ const Detail = (): JSX.Element => {
                     {memo?.bodyText}
                 </Text>
             </ScrollView>
-            <CircleButton onPress={handlePress} style={{top: 60, bottom: 'auto' }}>
+            <CircleButton onPress={() => {handlePress(id) }} style={{top: 60, bottom: 'auto' }}>
             <Icon name="pencil" size={40} color="#ffffff"/>
             </CircleButton>
         </View>
